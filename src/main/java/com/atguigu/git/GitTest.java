66c759cf5cf2bfb90970bd766352b8780222b73c
Rodddy <2978695809@qq.com>
2022-09-18 18:21:11 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,6 +2,8 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git1");
+        System.out.println("Hello Git2");
+
     }
 }
